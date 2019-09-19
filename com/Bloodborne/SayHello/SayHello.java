482a54a83c9c58b7c027fc685d7e4c7ed4ad41e6
num 95 <dfhdfgdg>
2019-09-19 21:32:32 +0800

    work

---
+++ com/Bloodborne/SayHello/SayHello.java
@@ -0,0 +1,20 @@
+/**
+*@program:
+*@descroption:xie sayHello de cheng xu guo cheng
+*@author:zhang
+*@data:9.19
+*/
+publice class SayHello{
+	publice static void main(String[] agrs){
+		System.out.println("Below is the funciton of sayhello");
+        sayHello(0);	
+	}
+	
+	public static void sayHello(omt sum){
+		System.out.println("say Hello!"+sum);
+	}
+	
+	public static int sum(){
+		return 1+3;
+	}
+}
