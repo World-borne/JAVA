8168f509350e75727bde6baf1c987cf01218ffbe
num 95 <dfhdfgdg>
2019-09-23 18:34:01 +0800

    daima

---
+++ Age/Age.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+public static Age{
+	public static void main(String[] agrs){
+		
+		
+		Scanner scan = new Scanner(System.in);
+		int age = scan.nextInt();
+
+
+		int check = age/18
+		switch(check){
+			case 0:
+			System.out.printf("I am a litter sister");
+			default:System.out.printf("I am an old aunt");
+			break;
+		}
+	}
+}
---
+++ Pyramid/Pyramid.java
@@ -0,0 +1,28 @@
+/**
+*@program:jin zi ta
+*@Description:shi yong fu hao lai cheng xian chu jin zi ta
+*@author:zhang
+*@Data:9.21
+*/
+public class Pyramid{
+	public static void main(String[] agrs);{
+		int i = 5;
+		int j = 1
+		for(j = 1;j<=1;j++)
+		{
+			int n = (2*j)-1;
+			int m = 0;
+			while(m =! (i-j))
+			{
+				System.out.printf(" ");
+				m++;
+			}
+			while(n =! 0)
+			{
+				System.out.printf("*");
+				n--;
+			}
+			System.out.printf("\n");
+		}
+	}
+}
