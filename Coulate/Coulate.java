0675649abdb43ee4321e1887fbd2eb2f1dc2e09a
num 95 <dfhdfgdg>
2019-09-23 19:09:06 +0800

    daima

---
+++ Coulate/Coulate.java
@@ -0,0 +1,5 @@
+public class Coulate{
+	public static void main(String[] agrs){
+		System.out.println("34 + 30"+34+30);
+	}
+}
---
+++ YunSuan/YunSuan.java
@@ -0,0 +1,9 @@
+public class YunSuan{
+	public static void main(String[] agrs){
+		int count = 10;
+		count *= count+2;
+		count /= 100;
+		cout++;
+		System.out.printf("cout"+cout);
+	}
+}
